891be30f81a8f6e038801dad1d5d637507831d3a
andrey <andrey@info-02.SERVER>
2023-04-17 11:42:45 -0300

    primeira commit

---
+++ interface.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("click", handleClick);
+  });
+});
+
+function handleClick(event) {
+  let square = event.target;
+  let position = square.id;
+  if (handleMove(position)) {
+    setTimeout(() => {
+      alert("Game Over! - O jogador " + playerTime + " foi o vencedor!");
+    }, 10);
+  }
+  updateSquare(position);
+}
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`;
+}
+
